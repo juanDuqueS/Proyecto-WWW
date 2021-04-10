97a857dceb4c40b04a21a9cc392d66e61dc2dd56
juanDuqueS <juan.duque.serna@correounivalle.edu.co>
2021-04-10 16:09:44 -0500

    starting stock app

--- Project_WWW/settings.py
+++ Project_WWW/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pbr8d#b4lw@%#81kde$c39y20jf@pnd*pgf^ar1rs)m(mya5)a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
