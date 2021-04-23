50cb5709552c2f4d07e93662be8510d97f35cac3
SanMejia <mejia.santiago@correounivalle.edu.co>
2021-04-23 17:05:58 -0500

    Cambio de color

--- gao-front/src/theme/index.js
+++ gao-front/src/theme/index.js
@@ -5,12 +5,12 @@ import typography from './typography';
 const theme = createMuiTheme({
   palette: {
     background: {
-      default: '#F4F6F8',
+      default: '#f8f7f4',
       paper: colors.common.white
     },
     primary: {
       contrastText: '#ffffff',
-      main: '#5664d2'
+      main: '#e6be6a'
     },
     text: {
       primary: '#172b4d',
