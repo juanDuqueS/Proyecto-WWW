8e785660bceec35c5c6832ca1e205e07f7b7270f
juanDuqueS <juan.duque.serna@correounivalle.edu.co>
2021-05-16 16:24:48 -0500

    starting django app

--- stock/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='stock-home'),
-    path('about/',views.about, name='stock-about')
-]
--- stock/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def home(request):
-    return HttpResponse('<h1>Stock home</h1>')
-
-def about(request):
-    return HttpResponse('<h1>Stock about<h1>')
