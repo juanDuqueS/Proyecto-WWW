6a3ec2ad2bd4bcd0981ffe90ca202e35f88e6873
juanDuqueS <juan.duque.serna@correounivalle.edu.co>
2021-05-17 17:31:13 -0500

    making templates

--- buildingManagement/admin.py
+++ buildingManagement/admin.py
@@ -1,9 +1,4 @@
-from buildingManagement.models import Build, Client, Employee
 from django.contrib import admin
 from buildingManagement import *
 
 # Register your models here.
-
-admin.site.register(Client)
-admin.site.register(Employee)
-admin.site.register(Build)
